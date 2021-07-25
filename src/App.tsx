c4925f6f766ce678289f9d561b0d947374d1e59f
Shad Reza <shadreza100@gmail.com>
2021-07-26 00:03:05 +0600

    Alhamdulillah context worked well, this was not working for the never thing being in the context so had to do some generic stuff to remove it

--- src/App.tsx
+++ src/App.tsx
@@ -3,56 +3,21 @@ import './App.css';
 import Header from './Components/Header/Header';
 
 // contexts
-export const UserNameContext1 = createContext('');
-export const UserNameContext2 = createContext({});
-export const UserLoggedInOrNotContext1 = createContext('');
-export const UserLoggedInOrNotContext2 = createContext({});
-export const UserImageContext1 = createContext('');
-export const UserImageContext2 = createContext({});
-
-// interface userInfo {
-//   name       : string;
-//   image      : string;
-//   email      : string;
-//   isLoggedIn : boolean;
-// }
-
-// type UserContextType = {
-//   user: userInfo;
-//   setUser: (props: userInfo) => void; 
-// };
-
-export const UserContext: object = createContext([])
+export let UserContext = createContext <any[]> ([])
 
 function App() {
 
   // states
   const [user, setUser] = useState({
-    name       : '',
+    name       : 'asdasd',
     email      : '',
     image      : '',
     isLoggedIn : false
   })
 
-  const [userName, setUserName] = useState('')
-  const [userLoggedInOrNot, setUserLoggedInOrNot] = useState('')
-  const [userImage, setUserImage] = useState('')
-
   return (
     <UserContext.Provider value={[user, setUser]}>
-    <UserNameContext1.Provider value={userName}>
-    <UserNameContext2.Provider value={setUserName}>
-    <UserLoggedInOrNotContext1.Provider value={userLoggedInOrNot}>
-    <UserLoggedInOrNotContext2.Provider value={setUserLoggedInOrNot}>
-    <UserImageContext1.Provider value={userImage}>
-    <UserImageContext2.Provider value={setUserImage}>
       <Header />
-    </UserImageContext2.Provider>
-    </UserImageContext1.Provider>
-    </UserLoggedInOrNotContext2.Provider>
-    </UserLoggedInOrNotContext1.Provider>
-    </UserNameContext2.Provider>
-    </UserNameContext1.Provider>
     </UserContext.Provider>
   );
 }
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,25 +1,19 @@
 import React, { useContext } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import { UserNameContext1, UserNameContext2, UserLoggedInOrNotContext1 } from '../../App';
+import './Header.css'
 import logoPic from '../Images/logo.png';
 import {
     BrowserRouter as Router,
-    Switch,
     Route,
+    Switch,
     Link
   } from "react-router-dom";
+import { UserContext } from '../../App';
 
 const Header = () => {
 
     // contexts
-    // const User = useContext(UserContext)
-    const user_name = useContext(UserNameContext1)
-    const set_user_name = useContext(UserNameContext2)
-    const user_logged_in_or_not = useContext(UserLoggedInOrNotContext1)
-
-    const fun = () => {
-        set_user_name('s')
-    }
+    const USER = useContext(UserContext)
 
     return (
         <Router>
@@ -30,19 +24,19 @@ const Header = () => {
                         src={logoPic}
                     />
                     {
-                        <strong>{user_name}'s Todo</strong>
+                        <strong>{USER[0].name}'s Todo</strong>
                     }
                 </div>
                 <div className="right">
                     <Link to="/">Home</Link>
                     <Link to="/about">About</Link>
                     {
-                        // User[0].isLoggedIn === true ? 
-                        //     <LazyLoadImage
-                        //         alt={User[0].name}
-                        //         src={User[0].image}
-                        //     />
-                        //     :
+                        USER[0].isLoggedIn === true ? 
+                            <LazyLoadImage
+                                alt={USER[0].name}
+                                src={USER[0].image}
+                            />
+                            :
                             <Link to="/login">Login</Link>
                     }
                 </div>
@@ -52,7 +46,3 @@ const Header = () => {
 };
 
 export default Header;
-
-function UserNameContext(UserNameContext: any) {
-    throw new Error('Function not implemented.');
-}
